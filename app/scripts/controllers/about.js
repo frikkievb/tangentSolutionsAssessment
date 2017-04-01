11ffdad39fae2408b86f611e21a24bc16e92474d
DESKTOP-DAFU0IM\frikk <frikkievb@gmail.com>
2017-04-01 21:38:29 +0200

    Initial Commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name tangentSolutionsAssessmentApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the tangentSolutionsAssessmentApp
+ */
+angular.module('tangentSolutionsAssessmentApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name tangentSolutionsAssessmentApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the tangentSolutionsAssessmentApp
+ */
+angular.module('tangentSolutionsAssessmentApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
